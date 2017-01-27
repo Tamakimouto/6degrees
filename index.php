24f0f9f19eff40c0c76b11ebd1c8a1984b74946f
Anthony Zheng <Anthony@fopen-dream.space>
2017-01-27 16:07:39 -0500

    ajax prep

--- index.php
+++ index.php
@@ -39,11 +39,15 @@
             <div class="row forms">
                 <div class="col-xs-12 col-sm-offset-3 col-sm-6">
 
+                    <!--
+
                     <div class="row">
                         <div class="col-xs-offset-3 col-xs-6">
                             <img class="img-responsive img-circle" src="img/kevinbacon3.jpg">
                         </div>
                     </div>
+
+                    -->
 
                     <ul class="nav nav-tabs">
                         <li class="active"><a data-toggle="tab" href="#1degree"> {{ tab1 }} </a></li>
@@ -53,18 +57,32 @@
                     <div class="tab-content">
                         <div class="tab-pane fade in active" id="1degree">
                             <p> {{ form1desc }} </p>
-                            <form action="php/1degree.php">
-                                <input class="text-center" type="text" name="actor" placeholder="Enter First and Last name">
-                                <input type="Submit" value="Search">
+                            <form>
+                                <input
+                                class="text-center"
+                                type="text"
+                                v-model.trim="name"
+                                @input="callAjax('php/test.php')"
+                                @keypress="preventSubmission"
+                                :placeholder="formprompt">
                             </form>
                         </div>
                         <div class="tab-pane fade in" id="2degree">
                             <p> {{ form2desc }} </p>
-                            <form action="php/2degree.php">
-                                <input class="text-center" type="text" name="actor" placeholder="Enter First and Last name">
-                                <input type="Submit" value="Search">
+                            <form>
+                                <input
+                                class="text-center"
+                                type="text"
+                                v-model.trim="name"
+                                @input="callAjax('php/2degree.php')"
+                                @keypress="preventSubmission"
+                                :placeholder="formprompt">
                             </form>
                         </div>
+                    </div>
+
+                    <div class="results">
+                        <!-- Results displayed here -->
                     </div>
 
                 </div>
--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ $(function() {
                     scrollTop: $("#homeContent").offset().top
                 }, 700);
                 $(".forms").addClass("animated").addClass("fadeIn");
-            }
+            } // goToMain
         }
     });
 
@@ -24,7 +24,42 @@ $(function() {
             tab1: "1st Degree",
             tab2: "2nd Degree",
             form1desc: "Search for all films with both a given actor and Kevin Bacon.",
-            form2desc: "Search for all films within 2 degrees with both a given actor and Kevin Bacon."
+            form2desc: "Search for all films within 2 degrees with both a given actor and Kevin Bacon.",
+            formprompt: "Enter First and Last name",
+            name: ""
+        },
+        methods: {
+            callAjax: function(filename) {
+                if (this.name.split(" ").length == 2) {
+                    var first = this.name.split(" ")[0];
+                    var last = this.name.split(" ")[1];
+                    home.ajax(first, last, filename);
+                } else if (this.name.split(" ").length == 3) {
+                    var first = this.name.split(" ")[0] + " " + this.name.split(" ")[1];
+                    var last = this.name.split(" ")[2];
+                    home.ajax(first, last, filename);
+                } else {
+                    $(".results").html("");
+                }
+            },
+            ajax: function(fname, lname, filename) {
+                $.ajax({
+                    type: "POST",
+                    dataType: "json",
+                    url: filename,
+                    data: {
+                        firstName: fname,
+                        lastName: lname
+                    },
+                    success: function(res) {
+                        $(".results").html(res["firstName"] + " " + res["lastName"]);
+                    }
+                });
+            },
+            preventSubmission: function(key) {
+                if (key.which == 13 )
+                    key.preventDefault();
+            }
         }
     });
 
